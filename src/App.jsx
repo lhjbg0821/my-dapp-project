eed582884e4f0c98657871cf0fc6a321e1c75cec
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-03-30 16:00:28 +0900

    Introduce

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <main>
         {/* Introduce */}
         <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
-          여기에 나를 소개하는 내용 넣기
+          Hello, My Name is HyunJin. I am Programmer.
         </div>
       </main>
     </div>
