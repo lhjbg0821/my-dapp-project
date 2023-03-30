98e4343155b58ec121260250db737f5ececd4160
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-03-30 15:56:44 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import PuppyLogo from "./images/puppy.png";
+
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-      <h2>My Name is 현진</h2>
-      <h3>My Favorite drink is Zero Coke❤️🍪 </h3>
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-100 h-12 md:h-20">
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+          {/* <div>HyunJin's PortFolio</div> */}
+          <div>
+            <img className="w-12" src={PuppyLogo} alt="Nike"></img>
+          </div>
+          <ul className="flex gap-2 md:gap-8 text-xs md:text-base">
+            <li className="bg-indigo-300 btn-style">Introduce</li>
+            <li className="bg-purple-300 btn-style">Portfolio</li>
+            <li className="bg-blue-300 btn-style">Contact Me</li>
+          </ul>
+        </div>
+      </header>
+      <main>
+        {/* Introduce */}
+        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          여기에 나를 소개하는 내용 넣기
+        </div>
+      </main>
     </div>
   );
 }
