e01419f14acd21c30aaf01f46e961123aa09e980
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-03-30 10:42:54 +0900

    Cookie

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ function App() {
     <div>
       <h1>Hello, React!</h1>
       <h2>My Name is 현진</h2>
-      <h3>My Favorite drink is Zero Coke </h3>
+      <h3>My Favorite drink is Zero Coke❤️ </h3>
     </div>
   );
 }
 
 export default App;
+
+/*
+commit 은 여러번 하고
+배포할 때가 됐다 싶으면 push
+*/
